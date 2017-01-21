b6ecb574f97f5a43b195557370a2e3ae945275a8
alvaropozor <alvaropozo@gmail.com>
2017-01-20 22:46:12 -0300

    se agregó texto definitivo y google analytics

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title><?php echo $titulo_pagina; ?></title>
+        <title>Frente Amplio - te invitamos a construir</title>
         <meta name="description" content="<?php echo $descripcion_pagina; ?>">  <!-- pendiente texto -->
         <meta property="og:title" content="<?php echo $titulo_share; ?>" />  <!-- pendiente texto -->
         <meta property="og:description" content="<?php echo $descripcion_share; ?>" />  <!-- pendiente texto -->
@@ -30,6 +30,16 @@
         <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
     </head>
     <body>
+        <!-- PENDIENTE Google Analytics-->
+        <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-90687967-1', 'auto');
+  ga('send', 'pageview');
+</script>
         <!--[if lt IE 8]>
             <p class="browserupgrade">Estás usando un <strong>navegador muy antiguo</strong>. Por favor (y por tu propia seguridad) <a href="http://browsehappy.com/">actualiza tu navegador</a>.</p>
         <![endif]-->
@@ -73,21 +83,23 @@ Frente Amplio
     <div class="container">
       <div class="row">
         <div class="col-md-8">
-          <h2>Titular de la convocatoria</h2>
+          <h2>Te invitamos a construir Frente Amplio</h2>
       <span class="textoConvocatoria">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id metus risus. Sed sed egestas velit, nec semper eros. Integer et rhoncus felis, vitae commodo sem. Donec placerat elit non magna hendrerit efficitur. Nulla venenatis maximus consequat. Integer tincidunt libero non dolor accumsan, quis volutpat lectus consequat. Curabitur ut condimentum enim.
-      </p>
-      <p>
-        Vivamus non sagittis nunc. Integer elit purus, gravida tempus lorem eget, vehicula iaculis felis. Etiam bibendum lacus ac massa viverra, et faucibus neque semper. Integer nibh justo, ullamcorper ut pellentesque in, finibus convallis lacus. Phasellus eget pellentesque odio, quis auctor est. Nulla at tellus laoreet, placerat nisl vitae, gravida diam. Nunc quis suscipit nunc, nec ultrices velit. Aliquam in magna eros. Integer venenatis magna eleifend dui sagittis, bibendum scelerisque metus ornare.
-      </p>
+<p>Nuestro país se encuentra frente al agotamiento del proceso de más de 26 años en el que se ha impedido la expresión de la soberanía popular en las instituciones y en la toma de decisiones públicas, lo que se ha traducido en el predominio del gran empresariado en contra de los intereses de la mayoría social. Este proceso fue incapaz de crear las condiciones necesarias para avanzar hacia un país con una democracia plena, capaz a su vez de superar las enormes desigualdades existentes en distintos ámbitos de la vida, pero en particular en aquellos relacionados con los derechos más básicos de las chilenas y chilenos. Son 26 años de una creciente elitización del sistema político, cada vez más ajeno a las grandes mayorías nacionales. Esto ha generado una enorme crisis de legitimidad y representatividad, donde los problemas de corrupción y colusión empresarial son sus expresiones más incómodas y visibles.</p>
 
-      <p>
-        Maecenas orci lectus, auctor nec nisi ac, sollicitudin pulvinar tellus. Suspendisse varius blandit posuere. Nulla lacinia eu lectus at tempor. Aliquam erat volutpat. Nunc at velit eget velit aliquet elementum. Maecenas mi tortor, pharetra at vehicula nec, pharetra at magna. Duis quis erat ut enim dapibus dignissim. Aliquam lorem nisi, pretium ac rhoncus nec, viverra vel ex. Morbi ullamcorper aliquam lacus sed semper. Nam laoreet sem in faucibus tempor. Etiam scelerisque blandit molestie. Etiam venenatis libero id sem tincidunt, a hendrerit nibh ultricies. Pellentesque tortor orci, ultrices vel aliquet nec, scelerisque at massa.
-      </p>
-      <p>
-        Nulla sit amet dignissim lectus. Cras velit nibh, faucibus auctor risus at, tincidunt cursus dolor. Aenean nisl nisl, pretium eget mi lobortis, feugiat auctor turpis. Integer posuere nisi cursus mi efficitur, et suscipit mi dapibus. Duis ante purus, bibendum et eleifend vel, ullamcorper vel velit. Donec ultrices malesuada nisi vitae sollicitudin. Sed ex dui, aliquam quis diam id, maximus cursus lacus. Proin nec feugiat nunc. Fusce nibh ante, tempor non semper nec, mattis nec odio. Pellentesque sed nisl risus. Quisque eget porttitor turpis, ac iaculis libero. Proin vulputate nibh sed mi viverra eleifend. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer ac semper enim, in congue metus. In hac habitasse platea dictumst.
-      </p>
+<p>Este creciente malestar en diferentes franjas sociales ha permitido la emergencia de un conjunto de liderazgos, fuerzas e iniciativas políticas y sociales que persiguen el objetivo de intervenir y participar del espacio político para transformar la sociedad y avanzar hacia más democracia, más libertades, más igualdad social e igualdad entre géneros y más protección de los ecosistemas que sustentan la vida en común. Por ahora, sin embargo, dichas instancias han operado de modo discontinuo, fragmentado y localizado, lo que ha impedido la constitución de un espacio político alternativo a los dos grandes bloques en el que pueda reconocerse la mayoría social, la que ha optado en una medida importante por sustraerse de la participación política. Es nuestra misión construir un punto de referencia para las iniciativas transformadoras. Por ello nos orientamos hacia la conformación de un movimiento político y social amplio que permita hacer converger, en su diversidad, todos los esfuerzos y las voluntades que permitan cambiar democráticamente la estructuras desiguales de poder en Chile, y que sea una fuerza política con opción real de gobernar y de legislar en beneficio de la mayoría social, antes que permanecer sólo en el terreno testimonial.</p>
+
+<p>Construir convergencia y unidad es probablemente uno de los desafíos más relevantes que tenemos las fuerzas transformadoras para el futuro inmediato.  Se trata de conformar un referente que supere el estrecho margen de una coalición electoral, con una identidad propia, en el que conviva la más amplia gama de visiones y organizaciones políticas, sociales e independientes. Necesitamos ante todo sumar voluntades de los miles de chilenos y chilenas que quieren cambios y que no se sienten representados por las coaliciones de hoy. Somos ciudadanos y organizaciones sociales y populares que queremos tomar el futuro en nuestras manos e invitar a ser de nuevo un pueblo con voluntad colectiva.</p>
+
+<p>Como primer paso de este largo camino, invitamos a la ciudadanía y las organizaciones sociales a construir Frente Amplio a lo largo de todo el país. Para desde este espacio constituir un pacto electoral unitario para enfrentar juntos las elecciones parlamentarias, de consejeros regionales y presidenciales venideras. </p>
+
+          <h2>NUESTROS PRINCIPIOS</h2>
+<ol>
+    <li>La conformación de una fuerza política y social transformadora cuyo propósito es superar el sistema neoliberal.</li>
+    <li>La unidad en la diversidad, con vocación participativa, democrática y plural.</li>
+    <li>Ser alternativa al duopolio conformado por la Derecha y la Nueva Mayoría.</li>
+    <li>Independencia total  del poder empresarial.</li>
+    <li>Un programa construido democráticamente como base de la unidad.</li>
 
         </span>
         </div>
@@ -176,14 +188,6 @@ Frente Amplio
         <script src="js/main.js"></script>
         <script src="js/submit_form.js"></script>
 
-        <!-- PENDIENTE Google Analytics-->
-        <script>
-            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            e.src='//www.google-analytics.com/analytics.js';
-            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
-        </script>
+    
     </body>
 </html>
