19eb9c40d162a72931c1785f718ecc4bc645ad6d
Deisy Quezada <doakym@gmail.com>
2017-01-21 22:16:37 -0300

    Se agrega código de facebook

--- index.php
+++ index.php
@@ -220,6 +220,15 @@
         <script src="js/main.js"></script>
 		
         <script src="js/submit_form.js"></script>
+	    <!-- Load Facebook SDK for JavaScript -->
+	<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.8&appId=2065792086980787";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 		
 		
 	</body>
